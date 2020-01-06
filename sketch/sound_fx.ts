5b5d7158cff48dabc22921348b8a77ce2279f7ce
Pontus Tahir <pontus.tahir@gmail.com>
2020-01-06 23:19:37 +0100

    correct

--- sketch/sound_fx.ts
+++ sketch/sound_fx.ts
@@ -1,6 +1,17 @@
-// let space: any
-
 class SoundEffects {
+        private swoosh: p5.SoundFile
+        private spaceClick: p5.SoundFile
+        private boooo: p5.SoundFile
+        private tadaa: p5.SoundFile
+        private yaaayy: p5.SoundFile
+
+        constructor() {
+                this.spaceClick = (window as any).loadSound('./assets/sounds/Stapler-SoundBible.com-374581609.mp3')
+                this.swoosh = (window as any).loadSound('./assets/sounds/Swoosh 3-SoundBible.com-1573211927.mp3')
+                this.boooo = (window as any).loadSound('./assets/sounds/boooo.mp3')
+                this.tadaa = (window as any).loadSound('./assets/sounds/tadaa.mp3')
+                this.yaaayy = (window as any).loadSound('./assets/sounds/yaaayy.mp3')
+        }
 
         public spaceBarSound() {
                 this.spaceClick.play()
